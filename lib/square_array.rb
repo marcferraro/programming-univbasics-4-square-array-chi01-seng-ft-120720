63f27fff97c66dedb88e08178857f3dc29263a0f
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-17 01:24:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,17 @@
 def square_array(array)
+<<<<<<< HEAD
   count = 0
   new_array = []
   while array[count] do
     new_array.push(array[count]**2)
     count += 1
+=======
+  counter = 0
+  new_array = []
+  while square_array(counter) do
+    new_array.push(array[counter]**2)
+    counter += 1
+>>>>>>> 753cd7fae008371c688f13b220f160079682d4e7
   end
  new_array
 end
